330dcd62f51f22084b282827207e2be44777423e
relevant <100879968+Guilded1@users.noreply.github.com>
2025-11-09 19:08:02 -0500

    updated toggle to Check

--- main.lua
+++ main.lua
@@ -1,91 +1,91 @@
 local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/Rain-Design/PPHUD/main/Library.lua'))()
 local lplr = game.Players.LocalPlayer
 local Window = Library:Window({
-    Text = "cracs",
+	Text = "cracs",
 })
 
 local Tab = Window:Tab({
-    Text = "Player"
+	Text = "Player"
 })
 
 local Section = Tab:Section({
-    Text = "Movement",
-    Side = "Left"
+	Text = "Movement",
+	Side = "Left"
 })
 
 
 local walkchanger = Section:Slider({
-    Text = "Speed",
-    Minimum = 0,
-    Maximum = 100,
-    Default = 16,
-    Postfix = "Walkspeed",
-    Callback = function(value)
-        if lplr and lplr.Character and lplr.Character:FindFirstChild("Humanoid") then
-            lplr.Character.Humanoid.WalkSpeed = value
-        end
-    end
+	Text = "Speed",
+	Minimum = 0,
+	Maximum = 100,
+	Default = 16,
+	Postfix = "ws",
+	Callback = function(value)
+		if lplr and lplr.Character and lplr.Character:FindFirstChild("Humanoid") then
+			lplr.Character.Humanoid.WalkSpeed = value
+		end
+	end
 })
 
 local noclip
-local phase = Section:Toggle({
-    Text = "Noclip",
-    Default = false,
-    Callback = function(value)
-        if value then
-            noclip = game:GetService('RunService').Stepped:Connect(function()
-                if lplr and lplr.Character then
-                    for _, part in lplr.Character:GetDescendants() do
-                        if part:IsA("BasePart") and part.CanCollide then
-                            part.CanCollide = false
-                        end
-                    end
-                end
-            end)
-        else
-            if value == false and noclip then
-                noclip:Disconnect()
-            end
-        end
-    end
+local phase = Section:Check({
+	Text = "Noclip",
+	Default = false,
+	Callback = function(value)
+		if value then
+			noclip = game:GetService('RunService').Stepped:Connect(function()
+				if lplr and lplr.Character then
+					for _, part in lplr.Character:GetDescendants() do
+						if part:IsA("BasePart") and part.CanCollide then
+							part.CanCollide = false
+						end
+					end
+				end
+			end)
+		else
+			if value == false and noclip then
+				noclip:Disconnect()
+			end
+		end
+	end
 })
 
 
 local antiafkconn
-local antiafk = Section:Toggle({
-    Text = "antiafk",
-    Default = false,
-    Callback = function(value)
-        if value then
-            if getconnections then
-                for _,v in next, getconnections(lplr.Idled) do
-                    if v["Disable"] then
-                        v["Disable"](v)
-                    elseif v["Disconnect"] then
-                        v["Disconnect"](v)
-                    end
-                end
-            else
-                antiafkconn = lplr.Idled:Connect(function()
-                    game:GetService("VirtualUser"):CaptureController()
-                    game:GetService("VirtualUser"):ClickButton2(Vector2.new())
-                end)
-            end
-        else
-            if value == false and antiafkconn then
-                antiafkconn:Disconnect()
-            end
-        end
-    end
+local antiafk = Section:Check({
+	Text = "antiafk",
+	Default = false,
+	Callback = function(value)
+		if value then
+			if getconnections then
+				for _,v in next, getconnections(lplr.Idled) do
+					if v["Disable"] then
+						v["Disable"](v)
+					elseif v["Disconnect"] then
+						v["Disconnect"](v)
+					end
+				end
+			else
+				antiafkconn = lplr.Idled:Connect(function()
+					game:GetService("VirtualUser"):CaptureController()
+					game:GetService("VirtualUser"):ClickButton2(Vector2.new())
+				end)
+			end
+		else
+			if value == false and antiafkconn then
+				antiafkconn:Disconnect()
+			end
+		end
+	end
 })
 
 local reset = Section:Button({
-    Text = "Reset",
-    callback = function(value)
-        if lplr and lplr.Character then
-            lplr.Character:BreakJoints()
-        end
-    end
+	Text = "Reset",
+	callback = function(value)
+		if lplr and lplr.Character then
+			lplr.Character:BreakJoints()
+		end
+	end
 })
 
 Tab:Select()
